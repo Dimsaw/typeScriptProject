b37703eb420593764a986a4d405c5cd42bda5f6d
Dima Savkov <dimsaw85@gmail.com>
2023-05-25 08:49:12 +0300

    work with types

--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -1,11 +1,32 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+let age: number;
+age = 50;
+
+let name: string;
+name = "Max";
+
+let toggle: boolean;
+toggle = true;
+
+let empty: null;
+empty = null;
+
+let notInitialize: undefined;
+notInitialize = undefined;
+
+let callback: (a: number) => void;
+
+callback = (a) => {
+  return 100 + a;
+};
